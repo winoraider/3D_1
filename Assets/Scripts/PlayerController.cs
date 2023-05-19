fda5bc1f68c4e4f1e9804b37023944d5ab2a0e5e
yari <ya12ri15@gmail.com>
2023-05-19 11:18:53 +0900

    PlayerControllerとCameraControllerを追加した。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        var movement = new Vector3(h,0,v);
+        rb.AddForce(movement);
+    }
+}
