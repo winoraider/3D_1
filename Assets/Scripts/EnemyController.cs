cbdc00bd603fd414dd5e1895f96fe44a1b115807
yari <ya12ri15@gmail.com>
2023-05-19 12:29:48 +0900

    Enemyを追尾するようにした。

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Vector3 dri;
+    public float speedE = 2;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dri = player.transform.position - transform.position;
+        rb.AddForce(dri * speedE);
+    }
+}
